499745985095e0e22e7e9ca5d1f572e095988b33
Yuriy Rybak <jura.ryback007@gmail.com>
2025-08-26 14:32:26 +0300

    Timer class has been moved to utils module.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 import logging
-import time
 from collections import Counter
-from typing import Type, Optional
-from types import TracebackType
 
 from app.settings import AppConfig, setup_logging
 from app.core.services.row_transformer import RowTransformer
@@ -16,31 +13,9 @@ from app.adapters.trip_data.resource_trip_data_provider import ResourceTripDataP
 from app.adapters.excel.pandas_excel_gateway import PandasExcelGateway
 
 from app.core.enums import ExcelColumns, CustomExcelColumns
+from app.utils import Timer
 
 logger = logging.getLogger(__name__)
-
-
-class Timer:
-    def __init__(self, label: str) -> None:
-        self.label = label
-        self.start_time: float = 0.0
-
-    def __enter__(self) -> "Timer":
-        self.start_time = time.perf_counter()
-        logger.info("[START] %s", self.label)
-        return self
-
-    def __exit__(
-            self,
-            exc_type: Optional[Type[BaseException]],
-            exc_value: Optional[BaseException],
-            traceback: Optional[TracebackType],
-    ) -> None:
-        elapsed = time.perf_counter() - self.start_time
-        if exc_type:
-            logger.error(f"[FAIL ] {self.label} — {elapsed:.2f}s")
-        else:
-            logger.info(f"[DONE ] {self.label} — {time.perf_counter() - self.start_time:.2f}s")
 
 
 def main() -> None:
